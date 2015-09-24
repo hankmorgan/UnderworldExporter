3576c359f6f2a2d1ffc7b059127ad0836044f546
hankmorgan <aidanontour@yahoo.co.uk>
2015-09-24 22:16:32 +0100

    Code for controlling the UW scroll dragons and the glowing eyes at the top of the screen

---
+++ UnityScripts/scripts/Dragons.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dragons : MonoBehaviour {
+
+
+	public UITexture[] Tail=new UITexture[4];
+	public UITexture[] Arm=new UITexture[4];
+	public UITexture[] Base=new UITexture[5];
+	public UITexture[] Head=new UITexture[4];
+
+	public int SetBase=0;
+	public int SetTail=0;
+	public int SetArm=0;
+	public int SetHead=0;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<=  Base.GetUpperBound(0);i++)
+		{
+			Base[i].enabled=false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateDragons(Base,SetBase);
+		UpdateDragons(Tail,SetTail);
+		UpdateDragons(Arm,SetArm);
+		UpdateDragons(Head,SetHead);
+	}
+
+
+	void UpdateDragons(UITexture[]parts, int EnabledIndex)
+	{
+		for (int i = 0; i<= parts.GetUpperBound(0);i++)
+		{
+			parts[i].enabled= (i==EnabledIndex);
+		}
+	}
+
+}
---
+++ UnityScripts/scripts/Eyes.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eyes : MonoBehaviour {
+
+	public int EyeIndex;
+	private int PreviousEyeIndex=-1;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Cycle",0.0f,1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (EyeIndex!=PreviousEyeIndex)
+		{
+			this.GetComponent<UITexture>().mainTexture=Resources.Load <Texture2D> ("HUD/Eyes/Eyes_"+ EyeIndex.ToString("0000"));
+		}
+	}
+
+	void Cycle()
+	{
+		if (EyeIndex >10)
+		{
+			EyeIndex=0;
+		}
+		else
+		{
+			EyeIndex++;
+		}
+
+	}
+}
